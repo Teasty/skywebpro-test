5032d94f0e411da1a855f92d7236c7f1784ada11
Андрей Лихачев <av-likhachev@yandex.ru>
2021-07-11 21:44:26 +0300

    Initial Commit

---
+++ skywebpro-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skywebpro-test
+//
+//  Created by Андрей Лихачев on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
